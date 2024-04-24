945774594e6341cce48cbd572d7dba918872d662
fruitptr <shaheerahmaddev@gmail.com>
2024-04-24 22:17:39 +0500

    Update requirements.txt

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from predict_jersey import predict_jersey
+from pydantic import BaseModel
+import json
+
+class model_input(BaseModel):
+  video : str
+  x : int
+  y : int
 
 app = FastAPI()
 
-@app.post("/predict/")
-async def predict_number(video:str, x:int, y:int):
-    tap_coords = (x, y)
-    number = predict_jersey(video, tap_coords)
-    return {"jersey_number": number}
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+@app.post("/predict/")
+async def predict_jersey_number(input_parameters : model_input):
+    input_data = input_parameters.json()
+    input_dictionary = json.loads(input_data)
+    tap_coords = (input_dictionary['x'], input_dictionary['y'])
+    number = predict_jersey(input_dictionary['video'], tap_coords)
+    return {"jersey_number": number}
